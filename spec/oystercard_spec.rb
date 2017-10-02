30a001d0fbc656e5884c5743b59bdcdef82e2044
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-02 17:48:52 +0100

    Top-up method tested and implemented

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,4 +3,8 @@ describe Oystercard do
   it "#balance should return 0 as a default when Oystercard is initialised" do
     expect(subject.balance).to eq 0
   end
+  it "#top_up should change the balance on the Oystercard" do
+    expect(subject.top_up(99)).to eq 99
+    expect(subject.top_up(22)).to eq 22
+  end
 end
