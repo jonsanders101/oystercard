d2a21716fcb2deb1de0c0da436f50a4fcb1822a9
Canace Wong <canacewongtoday@hotmail.com>
2017-10-03 06:38:58 -0700

    step5 refactor

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,11 @@
 require 'oystercard'
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
   it "#balance should return 0 as a default when Oystercard is initialised" do
-    expect(subject.balance).to eq 0
+    expect(oystercard.balance).to eq 0
   end
   it "#top_up should change the balance on the Oystercard" do
-    expect(subject.top_up(99)).to eq 99
-    expect(subject.top_up(22)).to eq 121
+    expect(oystercard.top_up(99)).to eq 99
+    expect(oystercard.top_up(22)).to eq 121
   end
 end
