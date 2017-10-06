be6449f368558314cdcaa1d5efc274294ca1c797
Aram Simonian <aram.contacts@gmail.com>
2017-10-06 09:43:32 +0100

    modified journey class, ready for updated tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -4,27 +4,21 @@ class Journey
 
   def initialize(oystercard)
     @oystercard = oystercard
+    @oystercard.journey_history << { entry_station: nil, exit_station: nil}
   end
 
   def tap_in(station)
     fail 'Insufficient funds' if @oystercard.insufficient_balance?
     @entry_station = station
-
     @oystercard.deduct(Oystercard::PENALTY_FARE)
 
-    @oystercard.journey_history << { entry_station: station, exit_station: nil}
+    @oystercard.journey_history[-1][:entry_station] = @entry_station
   end
 
   def tap_out(station)
-    @exit_station = station
-    if tapped_in?
-      @oystercard.journey_history[-1][:exit_station] = @exit_station
-      reimburse_fare(@entry_station, @exit_station)
-    else
-      @oystercard.deduct(Oystercard::PENALTY_FARE)
-      @oystercard.journey_history << { entry_station: nil, exit_station: @exit_station}
-    end
-    @entry_station = nil
+    @oystercard.journey_history[-1][:exit_station] = station
+    reimburse_fare(@entry_station, station) if tapped_in?
+    @oystercard.deduct(Oystercard::PENALTY_FARE) if !tapped_in?
   end
 
   def tapped_in?
@@ -32,6 +26,6 @@ class Journey
   end
 
   def reimburse_fare(@entry_station, @exit_station)
-
+    # to be done
   end
 end
