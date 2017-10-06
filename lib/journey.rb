45be36a02990585e1ea4f6edce3d83c30c4b75e3
Pablo <pabloandresvdl@gmail.com>
2017-10-06 13:24:55 +0100

    add tap_in method to Journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -9,23 +9,26 @@ class Journey
 
   def tap_in(station)
     fail 'Insufficient funds' if @oystercard.insufficient_balance?
-    @entry_station = station
-    @oystercard.deduct(Oystercard::PENALTY_FARE)
-
-    @oystercard.journey_history[-1][:entry_station] = @entry_station
+    # @entry_station = station
+    # @oystercard.deduct(Oystercard::PENALTY_FARE)
+    #
+    # @oystercard.journey_history[-1][:entry_station] = @entry_station
   end
-
-  def tap_out(station)
-    @oystercard.journey_history[-1][:exit_station] = station
-    reimburse_fare(@entry_station, station) if tapped_in?
-    @oystercard.deduct(Oystercard::PENALTY_FARE) if !tapped_in?
-  end
-
-  def tapped_in?
-    !!@entry_station
-  end
-
-  def reimburse_fare(@entry_station, @exit_station)
-    # to be done
-  end
+#
+#   def tap_out(station)
+#     @oystercard.journey_history[-1][:exit_station] = station
+#     reimburse_fare(@entry_station, station) if tapped_in?
+#     @oystercard.deduct(Oystercard::PENALTY_FARE) if !tapped_in?
+#   end
+#
+#
+#
+#   def reimburse_fare(_station, _station1)
+#     # to be done
+#   end
+#
+# private
+#   def tapped_in?
+#     !!@entry_station
+#   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,66 +1,23 @@
 require 'journey'
 
 describe Journey do
-  subject(:journey) { described_class.new }
-  let(:oystercard) { double(:oystercard, balance: 1) }
+
+  let(:oystercard) { double(:oystercard, :insufficient_balance? => false, :journey_history => [] ) }
+    subject(:journey) { described_class.new(oystercard) }
   # let(:entry_station) { double(:entry_station) }
   # let(:entry_station) { double(:exit_station) }
   let(:station) { double(:station) }
 
   describe "#tap_in" do
 
-    it '#card in use' do
-      journey.tap_in(station)
-      expect(journey).to be_in_journey
-    end
 
-    it '#tap_in raises error if balance below minimum' do
-      expect { oystercard.tap_in(station) }.to raise_error('Insufficient funds')
-    end
+      it 'expects to raise error if insufficient funds' do
+     #double(:oystercard, :insufficient_balance? => false)
+     allow(oystercard).to receive(:insufficient_balance?) {true}
+        expect{journey.tap_in(station)}.to raise_error 'Insufficient funds'
 
-    it 'remembers the entry station' do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-      oystercard.tap_in(station)
-      expect(oystercard.entry_station).to eq (station)
     end
-
-    it '#creates one journey' do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-      subject.tap_in(station)
-      subject.tap_out(station)
-      expect(oystercard.journey_history.length).to eq 1
-      end
 
   end
-
-  describe "#tap_out" do
-
-    it '#card not in use' do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-      subject.tap_in(station)
-      subject.tap_out(station)
-      expect(oystercard).to_not be_in_journey
-    end
-
-      it '#deducts mimumum_balance' do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-      subject.tap_in(station)
-      expect { oystercard.tap_out(station)}.to change {oystercard.balance}.by(- Oystercard::MINIMUM_BALANCE)
-    end
-
-    it 'forgets the entry station' do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-      oystercard.tap_in(station)
-      subject.tap_out(station)
-      expect(oystercard.entry_station).to eq nil
-    end
-
-    it '#records exit station' do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-      subject.tap_in(station)
-      expect(oystercard.tap_out(station)).to eq oystercard.journey_history[-1][:exit_station]
-    end
-  end
-
 
 end
